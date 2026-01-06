f8517812859657c636778ce6257ca3176cec387c
Ayush Anand <anandayush865@gmail.com>
2026-01-07 04:09:06 +0530

    comit

--- src/components/Payment.jsx
+++ src/components/Payment.jsx
@@ -54,12 +54,6 @@ export default function Payment() {
       description: "",
       icon: CreditCard,
     },
-    {
-      id: "cod",
-      name: "Cash on Delivery",
-      description: "",
-      icon: Truck,
-    }
   ];
 
   // Load Razorpay script
