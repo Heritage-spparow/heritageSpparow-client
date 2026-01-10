7ce6889e000e4f0c633910aa0cb9e33436532dc1
Ayush Anand <anandayush865@gmail.com>
2026-01-10 16:06:57 +0530

    ui fixes

--- src/utils/loudinary.js
+++ src/utils/loudinary.js
@@ -5,7 +5,7 @@ export const cloudinaryOptimize = (url, type = "detail") => {
     thumbnail: "f_auto,q_auto,c_fill,w_400",
     card: "f_auto,q_auto,c_fill,w_600",
     detail: "f_auto,q_auto:best,c_limit,w_1400",
-     hero: "f_auto,q_auto,w_1600,dpr_auto",
+    hero: "f_auto,q_auto,w_1600,dpr_auto",
     zoom: "f_auto,q_auto:best,c_limit,w_2000",
   };
 
